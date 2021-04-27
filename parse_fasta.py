71958f57e2479923dd18fd1f67dccd5e9bf10382
Luca Chang <82618674+LucaOnline@users.noreply.github.com>
2021-04-27 00:13:55 -0700

    Write fasta parser

---
+++ parse_fasta.py
@@ -0,0 +1,17 @@
+from typing import Dict
+
+
+def parse_fasta(filename: str) -> Dict[str, str]:
+    with open(filename, "r") as f:
+        data = f.read()
+
+    sequences = {}
+    currentKey = ""
+    for line in data.splitlines():
+        if line.startswith(">"):
+            currentKey = line[1:].split("\t")[0]  # Only key on the protein name
+        else:
+            if sequences.get(currentKey) is None:
+                sequences[currentKey] = ""
+            sequences[currentKey] += line.strip()
+    return sequences
