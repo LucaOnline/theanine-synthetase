17595f374832329a79d8f1055e62511e521d9086
Luca Chang <82618674+LucaOnline@users.noreply.github.com>
2021-04-27 00:15:00 -0700

    Move data index to separate file

---
+++ data_index.py
@@ -0,0 +1,12 @@
+LONGJING43 = "longjing_43_transcriptome.fas"
+TEA015198 = "tea015198.fas"
+
+DATA_URLS = {
+    LONGJING43: {
+        "url": "http://tpia.teaplant.org/web/Download/Transcriptome_data/Camellia_sinensis_var_sinensis_cv_Longjing_43_Trans.fas.gz",
+        "compression": "gzip",
+    },
+    TEA015198: {
+        "url": "http://tpia.teaplant.org/Download_Sequence?ID=TEA015198",
+    },
+}
--- download_data.py
+++ download_data.py
@@ -2,18 +2,7 @@ import gzip
 import os
 import requests
 
-LONGJING43 = "longjing_43_transcriptome.fas"
-TEA015198 = "tea015198.fas"
-
-DATA_URLS = {
-    LONGJING43: {
-        "url": "http://tpia.teaplant.org/web/Download/Transcriptome_data/Camellia_sinensis_var_sinensis_cv_Longjing_43_Trans.fas.gz",
-        "compression": "gzip",
-    },
-    TEA015198: {
-        "url": "http://tpia.teaplant.org/Download_Sequence?ID=TEA015198",
-    },
-}
+from data_index import DATA_URLS
 
 
 def download_data():
