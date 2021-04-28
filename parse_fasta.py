21ad17d2deaad916bfe6d80bfd86aa2478229a51
Luca Chang <82618674+LucaOnline@users.noreply.github.com>
2021-04-28 14:59:14 -0700

    Write much more efficient fasta parser

--- parse_fasta.py
+++ parse_fasta.py
@@ -1,17 +1,20 @@
-from typing import Dict
+from typing import Iterator, Tuple
 
 
-def parse_fasta(filename: str) -> Dict[str, str]:
+def parse_fasta(filename: str) -> Iterator[Tuple[str, str]]:
     with open(filename, "r") as f:
         data = f.read()
 
-    sequences = {}
+    currentSeq = []
     currentKey = ""
     for line in data.splitlines():
         if line.startswith(">"):
+            if currentKey != "":
+                yield currentKey, "".join(currentSeq)
+                currentSeq = []
             currentKey = line[1:].split()[0]  # Only key on the protein name
         else:
-            if sequences.get(currentKey) is None:
-                sequences[currentKey] = ""
-            sequences[currentKey] += line.strip().upper()
-    return sequences
+            currentSeq.append(line.strip().upper())
+
+    if currentKey != "":
+        yield currentKey, "".join(currentSeq)
