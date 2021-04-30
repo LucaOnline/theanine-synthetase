688216346acc4461a3b6550a9092846027a8f53a
Luca Chang <82618674+LucaOnline@users.noreply.github.com>
2021-04-30 10:04:06 -0700

    Try inverting nucleotides before searching

--- check_occurrences.py
+++ check_occurrences.py
@@ -1,5 +1,4 @@
 from typing import Iterator, Tuple
-from multiprocessing import Pool
 
 from data_index import CAMELLIA_GENOME, TEA015198
 from parse_fasta import parse_fasta
@@ -14,19 +13,30 @@ NUCLEOTIDE_INVERSIONS = {
 
 
 def get_data(filename: str) -> str:
+    """Gets the relative file path for the provided file."""
     return "./data/" + filename
 
 
 def invert_nucleotides(nucleotides: str) -> str:
+    """Inverts a nucelotide sequence."""
     return "".join([NUCLEOTIDE_INVERSIONS[n] for n in nucleotides])
 
 
-def check_occurrences(needle: str, haystacks: Iterator[Tuple[str, str]]) -> int:
-    with Pool() as pool:
-        return len(pool.map(lambda haystack: haystack[1].count(needle), haystacks))
+def check_occurrences(needle: str, haystacks: Iterator[str]) -> int:
+    """
+    Returns the number of occurrences of the provided needle
+    in the iterator of haystacks.
+    """
+    return sum(map(lambda haystack: haystack.count(needle), haystacks))
 
 
 if __name__ == "__main__":
     genome = parse_fasta(get_data(CAMELLIA_GENOME))
     cstsi_mrna = list(parse_fasta(get_data(TEA015198)))[0][1]
-    print(check_occurrences(invert_nucleotides(cstsi_mrna), genome))
+    print(
+        check_occurrences(
+            # Pass in only the actual dataset sequences, not sequence names
+            invert_nucleotides(cstsi_mrna),
+            map(lambda seq: seq[1], genome),
+        )
+    )
