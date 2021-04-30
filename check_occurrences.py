bb0b63ccb985344933603155fb376253168e9661
Luca Chang <82618674+LucaOnline@users.noreply.github.com>
2021-04-30 10:12:29 -0700

    Undo removing poly-A tail because it didn't help

--- check_occurrences.py
+++ check_occurrences.py
@@ -9,11 +9,6 @@ def get_data(filename: str) -> str:
     return "./data/" + filename
 
 
-def remove_poly_a_tail(nucleotides: str) -> str:
-    """Removes the polyadenylated tail from a nucelotide sequence."""
-    return nucleotides.rstrip("A")
-
-
 def check_occurrences(needle: str, haystacks: Iterator[str]) -> int:
     """
     Returns the number of occurrences of the provided needle
@@ -28,7 +23,7 @@ if __name__ == "__main__":
     print(
         check_occurrences(
             # Pass in only the actual dataset sequences, not sequence names
-            remove_poly_a_tail(cstsi_mrna),
+            cstsi_mrna,
             map(lambda seq: seq[1], genome),
         )
     )
