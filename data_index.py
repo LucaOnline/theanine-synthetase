ab32d6469a6634f12752b3ca9bb2935754c79698
Luca Chang <82618674+LucaOnline@users.noreply.github.com>
2021-05-10 14:48:56 -0700

    Update protein links

--- data_index.py
+++ data_index.py
@@ -3,9 +3,9 @@ GLUTAMINE_SYNTHETASE = "csgsi.fas"
 
 DATA_URLS = {
     THEANINE_SYNTHETASE: {
-        "url": "https://www.ncbi.nlm.nih.gov/sviewer/viewer.cgi?tool=portal&save=file&log$=seqview&db=nuccore&report=fasta&id=394987255&extrafeat=null&conwithfeat=on&hide-cdd=on",
+        "url": "https://www.ncbi.nlm.nih.gov/sviewer/viewer.cgi?tool=portal&save=file&log$=seqview&db=nuccore&report=fasta&id=1573295573&extrafeat=null&conwithfeat=on&hide-cdd=on",
     },
     GLUTAMINE_SYNTHETASE: {
-        "url": "https://www.ncbi.nlm.nih.gov/sviewer/viewer.cgi?tool=portal&save=file&log$=seqview&db=nuccore&report=fasta&id=1585710209&extrafeat=null&conwithfeat=on&hide-cdd=on",
+        "url": "https://www.ncbi.nlm.nih.gov/sviewer/viewer.cgi?tool=portal&save=file&log$=seqview&db=nuccore&report=fasta&id=161789847&extrafeat=null&conwithfeat=on&hide-cdd=on",
     },
 }
