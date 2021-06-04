8d6fa126b5e0e720ef567bca52d52e11a349b751
LucaOnline <82618674+LucaOnline@users.noreply.github.com>
2021-06-04 16:12:39 -0700

    Test variance for CsGSI and shuffled sequence

--- analysis.py
+++ analysis.py
@@ -1,7 +1,8 @@
 import os
+from random import shuffle
 
 from alignment import align_sequences
-from data_index import DATA_URLS, CSTSI
+from data_index import CSTSI, CSGSI
 from parse_fasta import parse_fasta
 
 
@@ -24,27 +25,39 @@ def get_output(filename: str) -> str:
 if __name__ == "__main__":
     cstsi_mrna = list(parse_fasta(get_data(CSTSI)))[0][1]
 
-    # Analyze all GS sequences downloaded
-    test_sequences = [seq for seq in DATA_URLS.keys() if seq != CSTSI]
-    for sequence in test_sequences:
-        print("Analyzing %s..." % sequence)
+    # Analyze CsGSI sequence
+    print("Analyzing %s..." % CSGSI)
 
-        test_seq = list(parse_fasta(get_data(sequence)))[0][1]
-        alignment_result = align_sequences(test_seq, cstsi_mrna, nucleotides=True)
+    test_seq = list(parse_fasta(get_data(CSGSI)))[0][1]
+    alignment_result = align_sequences(test_seq, cstsi_mrna, nucleotides=True)
 
-        print(alignment_result.clustered_mismatches(15))
+    print(
+        "Variance within clusters: "
+        + str(alignment_result.clustered_mismatch_variance(cluster_size=15))
+    )
 
-        largest_mismatch_pos, largest_mismatch = alignment_result.largest_mismatch()
-        percent_similarity = 1 - (
-            alignment_result.hamming_distance()
-            / alignment_result.get_alignment_length()
+    largest_mismatch_pos, largest_mismatch = alignment_result.largest_mismatch()
+    percent_similarity = 1 - (
+        alignment_result.hamming_distance() / alignment_result.get_alignment_length()
+    )
+
+    # Output Supplementary Data 4
+    make_output_dir()
+    with open(get_output(CSGSI + ".aln.txt"), "w+") as f:
+        f.write(alignment_result.format_result(line_length=100))
+    with open(get_output(CSGSI + ".meta.txt"), "w+") as f:
+        f.write(
+            f"Percent similarity: {percent_similarity}\nLargest mismatch location: {largest_mismatch_pos}\nLargest mismatch size: {largest_mismatch}bp\n"
         )
 
-        # Output Supplementary Data 4
-        make_output_dir()
-        with open(get_output(sequence + ".aln.txt"), "w+") as f:
-            f.write(alignment_result.format_result(line_length=100))
-        with open(get_output(sequence + ".meta.txt"), "w+") as f:
-            f.write(
-                f"Percent similarity: {percent_similarity}\nLargest mismatch location: {largest_mismatch_pos}\nLargest mismatch size: {largest_mismatch}bp\n"
-            )
+    # Test random sequence
+    rand_seq = test_seq.split()
+    shuffle(rand_seq)
+    rand_seq = "".join(rand_seq)
+
+    rand_alignment_result = align_sequences(rand_seq, cstsi_mrna, nucleotides=True)
+
+    print(
+        "Variance within clusters (shuffled sequence): "
+        + str(alignment_result.clustered_mismatch_variance(cluster_size=15))
+    )
