e604201cfc623eb6ec404addfa3534311077e252
LucaOnline <82618674+LucaOnline@users.noreply.github.com>
2021-06-04 21:47:22 -0700

    Improve output files

--- analysis.py
+++ analysis.py
@@ -36,7 +36,8 @@ if __name__ == "__main__":
 
     with open(get_output(CSGSI + ".meta.txt"), "w+") as f:
         f.write(
-            "Formatted metadata -- not for programmatic use.\n"
+            "Formatted metadata -- not for programmatic use.\n\n"
+            + "Information for alignment with CsTSI:\n\n"
             + f"Percent similarity: {percent_similarity}\n"
             + f"Largest mismatch location: {largest_mismatch_pos}\n"
             + f"Largest mismatch size: {largest_mismatch}bp\n"
