c8a36098a907fa409220353cbb17b23d3f321ec7
LucaOnline <82618674+LucaOnline@users.noreply.github.com>
2021-06-04 19:56:18 -0700

    Output cluster variance in main analysis

--- analysis.py
+++ analysis.py
@@ -1,6 +1,7 @@
 from alignment import align_sequences
 from data_index import CSTSI, CSGSI
 from dir_utils import get_data, make_output_dir, get_output
+from options import CLUSTER_COUNT
 from parse_fasta import parse_fasta
 
 
@@ -19,11 +20,19 @@ if __name__ == "__main__":
         alignment_result.hamming_distance() / alignment_result.get_alignment_length()
     )
 
+    print(
+        "Variance between clusters: "
+        + str(alignment_result.clustered_mismatch_variance(cluster_count=CLUSTER_COUNT))
+    )
+
     # Output Supplementary Data 4
     make_output_dir()
     with open(get_output(CSGSI + ".aln.txt"), "w+") as f:
         f.write(alignment_result.format_result(line_length=100))
     with open(get_output(CSGSI + ".meta.txt"), "w+") as f:
         f.write(
-            f"Percent similarity: {percent_similarity}\nLargest mismatch location: {largest_mismatch_pos}\nLargest mismatch size: {largest_mismatch}bp\n"
+            f"Percent similarity: {percent_similarity}\n"
+            + f"Largest mismatch location: {largest_mismatch_pos}\n"
+            + f"Largest mismatch size: {largest_mismatch}bp\n"
+            + f"Variance between clusters ({CLUSTER_COUNT} clusters): {alignment_result.clustered_mismatch_variance(cluster_count=CLUSTER_COUNT)}\n"
         )
