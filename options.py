f16fa7bfc94dbbc9c0456c9de387a2d52e9bdbdb
LucaOnline <82618674+LucaOnline@users.noreply.github.com>
2021-06-05 16:23:38 -0700

    Add trial, increase maximum clusters to 40

--- options.py
+++ options.py
@@ -2,7 +2,7 @@
 
 # Number of clusters to use in mismatch clustering variance analysis.
 # Each number here represents a separate set of trials.
-CLUSTER_COUNTS = [10, 20, 30]
+CLUSTER_COUNTS = [5, 10, 20, 40]
 
 # Number of simulations to perform.
 SIMULATION_COUNT = 10000
