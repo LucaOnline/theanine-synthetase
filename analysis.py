a2750f6aa1242a4da6fa0b9205edc7c95deadfd1
LucaOnline <82618674+LucaOnline@users.noreply.github.com>
2021-06-04 21:45:23 -0700

    Output mismatches and dump to JSON

--- analysis.py
+++ analysis.py
@@ -1,3 +1,5 @@
+import json
+
 from alignment import align_sequences
 from data_index import CSTSI, CSGSI
 from dir_utils import get_data, make_output_dir, get_output
@@ -20,19 +22,36 @@ if __name__ == "__main__":
         alignment_result.hamming_distance() / alignment_result.get_alignment_length()
     )
 
-    print(
-        "Variance between clusters: "
-        + str(alignment_result.clustered_mismatch_variance(cluster_count=CLUSTER_COUNT))
+    clustered_mismatches = alignment_result.clustered_mismatches(
+        cluster_count=CLUSTER_COUNT
+    )
+    clustered_mismatch_variance = alignment_result.clustered_mismatch_variance(
+        cluster_count=CLUSTER_COUNT
     )
 
     # Output Supplementary Data 4
     make_output_dir()
     with open(get_output(CSGSI + ".aln.txt"), "w+") as f:
         f.write(alignment_result.format_result(line_length=100))
+
     with open(get_output(CSGSI + ".meta.txt"), "w+") as f:
         f.write(
-            f"Percent similarity: {percent_similarity}\n"
+            "Formatted metadata -- not for programatic use.\n"
+            + f"Percent similarity: {percent_similarity}\n"
             + f"Largest mismatch location: {largest_mismatch_pos}\n"
             + f"Largest mismatch size: {largest_mismatch}bp\n"
-            + f"Variance between clusters ({CLUSTER_COUNT} clusters): {alignment_result.clustered_mismatch_variance(cluster_count=CLUSTER_COUNT)}\n"
+            + f"Variance between clusters ({CLUSTER_COUNT} clusters): {clustered_mismatch_variance}\n"
+            + f"Clustered mismatches: {clustered_mismatches}\n"
+        )
+
+    with open(get_output(CSGSI + ".meta.json"), "w+") as f:
+        json.dump(
+            {
+                "percent_similarity": percent_similarity,
+                "largest_mismatch_pos": largest_mismatch_pos,
+                "largest_mismatch": largest_mismatch,
+                "clustered_mismatch_variance": clustered_mismatch_variance,
+                "clustered_mismatches": clustered_mismatches,
+            },
+            f,
         )
