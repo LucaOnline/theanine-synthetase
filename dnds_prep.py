01d3a8535d63851e5d0850679f404b0e89e01e26
LucaOnline <82618674+LucaOnline@users.noreply.github.com>
2021-06-05 17:02:07 -0700

    Fix indel trimming function

--- dnds_prep.py
+++ dnds_prep.py
@@ -12,6 +12,15 @@ def trim_indels(alignment: AlignmentResult) -> Tuple[str, str]:
     Trims indels from the provided alignment and returns a
     tuple (alignment_1, alignment_2).
     """
+
     alignment_1 = alignment.get_alignment_1()
     alignment_2 = alignment.get_alignment_2()
-    return alignment_1.replace("-", ""), alignment_2.replace("-", "")
+
+    indels_1 = [i for i, c in enumerate(alignment_1) if c == "-"]
+    indels_2 = [i for i, c in enumerate(alignment_2) if c == "-"]
+    indels_all = set(indels_1 + indels_2)
+
+    trimmed_1 = "".join([c for i, c in enumerate(alignment_1) if not i in indels_all])
+    trimmed_2 = "".join([c for i, c in enumerate(alignment_2) if not i in indels_all])
+
+    return trimmed_1, trimmed_2
